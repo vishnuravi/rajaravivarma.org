48dfc3d8de8feb808a5eb0f517486d1e52dc0109
Vishnu Ravi <vishnur@stanford.edu>
2026-05-05 10:02:18 -0700

    Hide footer Wander section on mobile

--- data.js
+++ data.js
@@ -160,7 +160,7 @@ window.RRV_renderChrome = function() {
           <h4>About this tribute</h4>
           <p>This site is a personal tribute to Raja Ravi Varma (1848–1906), maintained by his great-great-grandson <a href="https://vishnu.io" target="_blank" rel="noopener">Vishnu Ravi</a>. It gathers the paintings, the press, and the life, a descendant's offering to a great-great-grandfather's monumental work.</p>
         </div>
-        <div>
+        <div class="foot-wander">
           <h4>Wander</h4>
           <ul>
             <li><a href="index.html">Home</a></li>
